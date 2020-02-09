57c1d0eaf0aec01e90f195420ae99737a716a7cf
Jonathan Raines <jonathan.s.raines@gmail.com>
2020-02-08 22:47:41 -0600

    added comments and built out HTML further

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require("express"),
-    app     = express(),
-    bodyParser = require("body-parser");
+var bodyParser = require("body-parser"),
+    express    = require("express"),
+    app        = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,7 +11,7 @@ app.get("/", function(req,res) {
     res.render("index");
 });
 
-/* SHOW ROUTE - Show All Work Orders */
+/* SHOW ROUTE - Show All Work Orders OR Individual Specific Work Order Details */
 app.get("/workorders", function(req, res) {
     res.render("workOrder");
 });
@@ -21,6 +21,7 @@ app.get("/zookeepers", function(req, res) {
     res.render("zookeepers");
 });
 
+/* Pretty sure this and these next two routes will be redundant and one will need to be depracated */
 /* NEW ROUTE - Create a New Work Order*/
 app.get("/workorders/new", function(req, res) {
     res.render("newOrder");
@@ -31,6 +32,8 @@ app.post("/workorders", function(req,res) {
     res.redirect("/workorders");
 });
 
+
+/* Code to start server and view HTML in web browser */
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("The Server Has Started!");
