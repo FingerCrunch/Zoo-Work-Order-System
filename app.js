7dffc30a947cfae1b6a2f551a6a4fc0ac8829390
jsr8671 <jonathan.s.raines@gmail.com>
2020-02-10 02:48:20 -0600

    implemented db backend to fill info in ejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var bodyParser = require("body-parser"),
     express    = require("express"),
-    app        = express();
+    app        = express(),
+    mysql      = require("mysql"),
+    pool       = require("./pool");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -8,7 +10,35 @@ app.use(express.static(__dirname + "/public"));
 
 /* INDEX ROUTE - Home Page */
 app.get("/", function(req,res) {
-    res.render("index");
+    let zookeepers = [], supplies = [], enclosures = [];
+    pool.query('SELECT * FROM Zoo_Keepers WHERE onshift_status = 1', function(err1, rows1, field1) {
+        if(err1) {
+            console.log(err1);
+        }
+        for (let i in rows1) {
+            zookeepers.push(rows1[i]);
+            console.log(zookeepers[i]);
+        }
+        pool.query('SELECT * FROM Supplies', function(err2, rows2, field2) {
+            if(err2) {
+                console.log(err2);
+            }
+            for (let i in rows2) {
+                supplies.push(rows2[i]);
+                console.log(supplies[i]);
+            }
+            pool.query('SELECT * FROM Animal_Enclosures', function(err3, rows3, field3) {
+                if(err3) {
+                    console.log(err3);
+                }
+                for (let i in rows3) {
+                    enclosures.push(rows3[i]);
+                    console.log(enclosures[i]);
+                }
+                res.render("index", {zookeepers: zookeepers, supplies: supplies, enclosures: enclosures}); 
+            }); 
+        });
+    });
 });
 
 /* SHOW ROUTE - Show All Work Orders OR Individual Specific Work Order Details */
@@ -18,18 +48,34 @@ app.get("/workorders", function(req, res) {
 
 /* SHOW ROUTE - Show All Zoo Keepers */
 app.get("/zookeepers", function(req, res) {
-    res.render("zookeepers");
+    let zookeepers = [];
+    pool.query('SELECT * FROM Zoo_Keepers', function(err, rows, field) {
+        if(err) {
+            console.log(err);
+        }
+        for (let i in rows) {
+            zookeepers.push(rows[i]);
+            console.log(zookeepers[i]);
+        }
+        res.render("zookeepers", {zookeepers: zookeepers});
+    });
 });
 
-/* Pretty sure this and these next two routes will be redundant and one will need to be depracated */
-/* NEW ROUTE - Create a New Work Order*/
-app.get("/workorders/new", function(req, res) {
-    res.render("newOrder");
-});
-
+/* Not working yet */
 /* NEW ROUTE - Push New Work Order to DB */
 app.post("/workorders", function(req,res) {
-    res.redirect("/workorders");
+    let zookeeper = req.body.zookeeper,
+        enclosure = req.body.enclosure,
+        task      = req.body.task,
+        supply    = req.body.supplies,
+        available = true,
+        overdue   = false,
+        accepted = true;
+    
+    let workOrder = [];
+    pool.query('INSERT INTO Work_Orders (zookeeper_id, enclosure_id, task_name, supply_id, available, overdue_status, accepted) VALUES (zookeeper, enclosure, task, supply, available, overdue, accepted)', function(err, rows, field) {
+     
+    });
 });
 
 
